8071ca2facd9f63ab48c62acee01b106ee0cd394
Alvaro Duran <alvaroduran.96@gmail.com>
2018-05-17 17:09:10 +0200

    Añadido método createList

--- concesionario/src/main/java/evolsoft/concesionario/service/impl/CocheServiceImpl.java
+++ concesionario/src/main/java/evolsoft/concesionario/service/impl/CocheServiceImpl.java
@@ -149,5 +149,10 @@ public class CocheServiceImpl implements CocheService {
 		Date today = Calendar.getInstance().getTime();
 		return today.toString();
 	}
-
+@Override
+public void createList(List<CocheDTO> listCocheDto) {
+for(CocheDTO cocheDTO : listCocheDto) {
+cocheDAO.save(map(cocheDTO));
+}
+}
 }
